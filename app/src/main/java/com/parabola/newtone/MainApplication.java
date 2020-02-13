7108aa0c0fb24c9dea9ff420e5f13496ea717a3d
Khalikov Arslan <zizik.zizik@gmail.com>
2020-02-13 12:20:04 +0300

    Добавлена возможность отправки стектрейса при возникновении ошибок

--- app/src/main/java/com/parabola/newtone/MainApplication.java
+++ app/src/main/java/com/parabola/newtone/MainApplication.java
@@ -1,6 +1,7 @@
 package com.parabola.newtone;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.parabola.domain.interactors.player.PlayerInteractor;
 import com.parabola.domain.interactors.type.Irrelevant;
@@ -8,13 +9,27 @@ import com.parabola.newtone.di.ComponentFactory;
 import com.parabola.newtone.di.app.AppComponent;
 import com.parabola.newtone.ui.HomeScreenWidget;
 
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraMailSender;
+import org.acra.annotation.AcraToast;
+
 import io.reactivex.Observable;
 
+@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraMailSender(mailTo = "zizik.zizik@gmail.com")
+@AcraToast(resText = R.string.toast_text_on_crash)
 public final class MainApplication extends Application {
     private static final String LOG_TAG = "Newtone Application";
 
     private static AppComponent appComponent;
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //инициализация отправки сообщений об ошибке
+        ACRA.init(this);
+    }
 
     @Override
     public void onCreate() {
