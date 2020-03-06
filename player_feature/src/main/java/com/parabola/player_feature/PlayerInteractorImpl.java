3339ed3e0aa167624ee98f05b79112be741ef2a9
Khalikov Arslan <zizik.zizik@gmail.com>
2020-03-06 16:06:04 +0300

    Обновление до 0.1.0

--- player_feature/src/main/java/com/parabola/player_feature/PlayerInteractorImpl.java
+++ player_feature/src/main/java/com/parabola/player_feature/PlayerInteractorImpl.java
@@ -42,7 +42,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
