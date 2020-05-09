faa098f3e28671e4d02f9713fbfddb453addeec5
Khalikov Arslan <zizik.zizik@gmail.com>
2020-05-09 13:00:18 +0300

    Удалён CustomAddToEndSingleStrategy

--- app/src/main/java/com/parabola/newtone/mvp/view/ArtistView.java
+++ app/src/main/java/com/parabola/newtone/mvp/view/ArtistView.java
@@ -2,14 +2,13 @@ package com.parabola.newtone.mvp.view;
 
 import com.parabola.domain.model.Album;
 import com.parabola.domain.settings.ViewSettingsInteractor.AlbumItemView;
-import com.parabola.newtone.mvp.CustomAddToEndSingleStrategy;
 
 import java.util.List;
 
 import moxy.MvpView;
-import moxy.viewstate.strategy.StateStrategyType;
+import moxy.viewstate.strategy.alias.AddToEndSingle;
 
-@StateStrategyType(CustomAddToEndSingleStrategy.class)
+@AddToEndSingle
 public interface ArtistView extends MvpView {
     void setArtistName(String artistName);
     void setTracksCount(int tracksCount);
--- app/src/main/java/com/parabola/newtone/mvp/view/TabAlbumView.java
+++ app/src/main/java/com/parabola/newtone/mvp/view/TabAlbumView.java
@@ -2,14 +2,13 @@ package com.parabola.newtone.mvp.view;
 
 import com.parabola.domain.model.Album;
 import com.parabola.domain.settings.ViewSettingsInteractor.AlbumItemView;
-import com.parabola.newtone.mvp.CustomAddToEndSingleStrategy;
 
 import java.util.List;
 
 import moxy.MvpView;
-import moxy.viewstate.strategy.StateStrategyType;
+import moxy.viewstate.strategy.alias.AddToEndSingle;
 
-@StateStrategyType(CustomAddToEndSingleStrategy.class)
+@AddToEndSingle
 public interface TabAlbumView extends MvpView {
     void refreshAlbums(List<Album> albums);
     void setSectionShowing(boolean enable);
