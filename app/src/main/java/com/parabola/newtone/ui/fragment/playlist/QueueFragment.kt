fe6d87e8ed6a42b42c30f842326892d6d0f14905
Arslan Khalikov <zizik.zizik@gmail.com>
2022-06-16 18:29:13 +0300

    Экран QueueFragment переведён на котлин

--- app/src/main/java/com/parabola/newtone/ui/fragment/playlist/QueueFragment.kt
+++ app/src/main/java/com/parabola/newtone/ui/fragment/playlist/QueueFragment.kt
@@ -1,135 +1,120 @@
-package com.parabola.newtone.ui.fragment.playlist;
+package com.parabola.newtone.ui.fragment.playlist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.parabola.domain.model.Track
+import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView
+import com.parabola.domain.utils.TracklistTool.isTracklistsIdentical
+import com.parabola.newtone.MainApplication
+import com.parabola.newtone.R
+import com.parabola.newtone.adapter.TrackAdapter
+import com.parabola.newtone.databinding.ListTrackBinding
+import com.parabola.newtone.mvp.presenter.QueuePresenter
+import com.parabola.newtone.mvp.view.QueueView
+import com.parabola.newtone.ui.base.BaseSwipeToBackFragment
+import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
 
-import static com.parabola.domain.utils.TracklistTool.isTracklistsIdentical;
+class QueueFragment : BaseSwipeToBackFragment(),
+    QueueView {
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+    @InjectPresenter
+    lateinit var presenter: QueuePresenter
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.DividerItemDecoration;
+    private var _binding: ListTrackBinding? = null
+    private val binding get() = _binding!!
 
-import com.parabola.domain.model.Track;
-import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView;
-import com.parabola.newtone.MainApplication;
-import com.parabola.newtone.R;
-import com.parabola.newtone.adapter.TrackAdapter;
-import com.parabola.newtone.databinding.ListTrackBinding;
-import com.parabola.newtone.di.app.AppComponent;
-import com.parabola.newtone.mvp.presenter.QueuePresenter;
-import com.parabola.newtone.mvp.view.QueueView;
-import com.parabola.newtone.ui.base.BaseSwipeToBackFragment;
+    private val queueAdapter = TrackAdapter()
+    private lateinit var itemDecoration: DividerItemDecoration
 
-import java.util.List;
-import java.util.OptionalInt;
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = super.onCreateView(inflater, container, savedInstanceState)
+        _binding = ListTrackBinding.inflate(inflater, container, false)
+        rootBinding.container.addView(binding.root)
+        rootBinding.main.setText(R.string.playlist_queue)
 
-import moxy.presenter.InjectPresenter;
-import moxy.presenter.ProvidePresenter;
+        binding.tracksList.adapter = queueAdapter
+        itemDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
 
-public final class QueueFragment extends BaseSwipeToBackFragment
-        implements QueueView {
+        queueAdapter.setMoveItemIconVisibility(true)
+        queueAdapter.setOnItemClickListener { position: Int ->
+            presenter.onClickTrackItem(queueAdapter.all, position)
+        }
+        queueAdapter.setOnMoveItemListener(presenter::onMoveItem)
+        queueAdapter.setOnSwipeItemListener { position: Int ->
+            queueAdapter.remove(position)
+            presenter.onRemoveItem(position)
+        }
+        rootBinding.actionBar.setOnClickListener { presenter.onClickActionBar() }
 
-    @InjectPresenter QueuePresenter presenter;
+        return root
+    }
 
-    private ListTrackBinding binding;
-
-    private final TrackAdapter queueAdapter = new TrackAdapter();
-    private DividerItemDecoration itemDecoration;
-
-    public QueueFragment() {
-        // Required empty public constructor
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
-    @NonNull
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View root = super.onCreateView(inflater, container, savedInstanceState);
-        binding = ListTrackBinding.inflate(inflater, container, false);
-        getRootBinding().container.addView(binding.getRoot());
-
-        getRootBinding().main.setText(R.string.playlist_queue);
-
-        binding.tracksList.setAdapter(queueAdapter);
-        itemDecoration = new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
-
-        queueAdapter.setMoveItemIconVisibility(true);
-        queueAdapter.setOnItemClickListener(position -> presenter.onClickTrackItem(queueAdapter.getAll(), position));
-        queueAdapter.setOnMoveItemListener(presenter::onMoveItem);
-        queueAdapter.setOnSwipeItemListener(position -> {
-            queueAdapter.remove(position);
-            presenter.onRemoveItem(position);
-        });
-        getRootBinding().actionBar.setOnClickListener(v -> presenter.onClickActionBar());
-
-        return root;
+    override fun onClickBackButton() {
+        presenter.onClickBack()
     }
 
-    @Override
-    protected void onClickBackButton() {
-        presenter.onClickBack();
-    }
-
-    @Override
-    protected void onEndSlidingAnimation() {
-        presenter.onEnterSlideAnimationEnded();
+    override fun onEndSlidingAnimation() {
+        presenter.onEnterSlideAnimationEnded()
     }
 
     @ProvidePresenter
-    public QueuePresenter providePresenter() {
-        AppComponent appComponent = ((MainApplication) requireActivity().getApplication()).getAppComponent();
-        return new QueuePresenter(appComponent);
+    fun providePresenter(): QueuePresenter {
+        val appComponent = (requireActivity().application as MainApplication).appComponent
+        return QueuePresenter(appComponent)
     }
 
-    @Override
-    public void refreshTracks(List<Track> tracks) {
-        if (!isTracklistsIdentical(tracks, queueAdapter.getAll())) {
-            queueAdapter.replaceAll(tracks);
+    override fun refreshTracks(tracks: List<Track>) {
+        if (!isTracklistsIdentical(tracks, queueAdapter.all)) {
+            queueAdapter.replaceAll(tracks)
         }
     }
 
-    @Override
-    public void setItemViewSettings(TrackItemView viewSettings) {
-        queueAdapter.setViewSettings(viewSettings);
+    override fun setItemViewSettings(viewSettings: TrackItemView) {
+        queueAdapter.setViewSettings(viewSettings)
     }
 
-    @Override
-    public void setItemDividerShowing(boolean showed) {
-        binding.tracksList.removeItemDecoration(itemDecoration);
-
-        if (showed)
-            binding.tracksList.addItemDecoration(itemDecoration);
+    override fun setItemDividerShowing(showed: Boolean) {
+        binding.tracksList.removeItemDecoration(itemDecoration)
+        if (showed) binding.tracksList.addItemDecoration(itemDecoration)
     }
 
+    override fun setTrackCount(tracksCount: Int) {
+        val tracksCountStr = resources
+            .getQuantityString(R.plurals.tracks_count, tracksCount, tracksCount)
 
-    @Override
-    public void setTrackCount(int tracksCount) {
-        String tracksCountStr = getResources()
-                .getQuantityString(R.plurals.tracks_count, tracksCount, tracksCount);
-
-        getRootBinding().additionalInfo.setText(tracksCountStr);
+        rootBinding.additionalInfo.text = tracksCountStr
     }
 
-    @Override
-    public void setCurrentTrackPosition(int currentTrackPosition) {
-        OptionalInt oldSelectedPosition = queueAdapter.getSelectedPosition();
-        if (oldSelectedPosition.isPresent()
-                && oldSelectedPosition.getAsInt() == currentTrackPosition) {
-            return;
+    override fun setCurrentTrackPosition(currentTrackPosition: Int) {
+        val oldSelectedPosition = queueAdapter.selectedPosition
+        if (oldSelectedPosition.isPresent
+            && oldSelectedPosition.asInt == currentTrackPosition
+        ) {
+            return
         }
         try {
-            queueAdapter.setSelected(currentTrackPosition);
-        } catch (IndexOutOfBoundsException ignored) {
-            queueAdapter.clearSelected();
+            queueAdapter.setSelected(currentTrackPosition)
+        } catch (e: IndexOutOfBoundsException) {
+            queueAdapter.clearSelected()
         }
     }
 
-    @Override
-    public void goToItem(int itemPosition) {
-        binding.tracksList.scrollToPosition(itemPosition);
-        binding.tracksList.stopScroll();
+    override fun goToItem(itemPosition: Int) {
+        binding.tracksList.scrollToPosition(itemPosition)
+        binding.tracksList.stopScroll()
     }
 }
