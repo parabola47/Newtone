8e073878c426e6b447d3a7ff0c6afa868c3ecd7c
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:28:37 +0300

    QueueView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/QueueView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/QueueView.kt
@@ -1,24 +1,20 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import com.parabola.domain.model.Track;
-import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView;
-
-import java.util.List;
-
-import moxy.MvpView;
-import moxy.viewstate.strategy.alias.AddToEndSingle;
-import moxy.viewstate.strategy.alias.OneExecution;
+import com.parabola.domain.model.Track
+import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
+import moxy.viewstate.strategy.alias.OneExecution
 
 @AddToEndSingle
-public interface QueueView extends MvpView {
+interface QueueView : MvpView {
+    fun setCurrentTrackPosition(currentTrackPosition: Int)
+    fun refreshTracks(tracks: List<Track>)
+    fun setTrackCount(tracksCount: Int)
 
-    void refreshTracks(List<Track> tracks);
-    void setItemViewSettings(TrackItemView viewSettings);
-    void setItemDividerShowing(boolean showed);
-
-    void setTrackCount(int tracksCount);
-    void setCurrentTrackPosition(int currentTrackPosition);
+    fun setItemViewSettings(viewSettings: TrackItemView)
+    fun setItemDividerShowing(showed: Boolean)
 
     @OneExecution
-    void goToItem(int itemPosition);
+    fun goToItem(itemPosition: Int)
 }
