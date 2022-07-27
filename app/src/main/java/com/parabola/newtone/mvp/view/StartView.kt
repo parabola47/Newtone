be993f909004b189c42ba966aa7b4f2e87bbb300
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:04:16 +0300

    StartView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/StartView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/StartView.kt
@@ -1,10 +1,9 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import moxy.MvpView;
-import moxy.viewstate.strategy.AddToEndSingleStrategy;
-import moxy.viewstate.strategy.StateStrategyType;
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
 
-@StateStrategyType(AddToEndSingleStrategy.class)
-public interface StartView extends MvpView {
-    void setPermissionPanelVisibility(boolean visible);
+@AddToEndSingle
+interface StartView : MvpView {
+    fun setPermissionPanelVisibility(visible: Boolean)
 }
