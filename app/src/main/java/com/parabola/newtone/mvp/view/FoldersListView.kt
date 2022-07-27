5c73f1652169aa11156c64e2c2d15169cc63f297
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:30:59 +0300

    FoldersListView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/FoldersListView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/FoldersListView.kt
@@ -1,15 +1,12 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import com.parabola.domain.model.Folder;
-
-import java.util.List;
-
-import moxy.MvpView;
-import moxy.viewstate.strategy.alias.AddToEndSingle;
+import com.parabola.domain.model.Folder
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
 
 @AddToEndSingle
-public interface FoldersListView extends MvpView {
+interface FoldersListView : MvpView {
+    fun refreshFolders(folders: List<Folder>)
 
-    void refreshFolders(List<Folder> folders);
-    void setItemDividerShowing(boolean showed);
+    fun setItemDividerShowing(showed: Boolean)
 }
