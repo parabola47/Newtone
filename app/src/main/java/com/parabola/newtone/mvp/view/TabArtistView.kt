3a1c47db21b311c8bbe209a6a54d5a8726d1cb4b
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:09:04 +0300

    TabArtistView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/TabArtistView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/TabArtistView.kt
@@ -1,19 +1,15 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import com.parabola.domain.model.Artist;
-import com.parabola.domain.settings.ViewSettingsInteractor.ArtistItemView;
-
-import java.util.List;
-
-import moxy.MvpView;
-import moxy.viewstate.strategy.alias.AddToEndSingle;
+import com.parabola.domain.model.Artist
+import com.parabola.domain.settings.ViewSettingsInteractor.ArtistItemView
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
 
 @AddToEndSingle
-public interface TabArtistView extends MvpView {
+interface TabArtistView : MvpView {
+    fun refreshArtists(artists: List<Artist>)
+    fun setItemViewSettings(viewSettings: ArtistItemView)
+    fun setItemDividerShowing(showed: Boolean)
 
-    void refreshArtists(List<Artist> artists);
-    void setItemViewSettings(ArtistItemView viewSettings);
-    void setItemDividerShowing(boolean showed);
-
-    void setSectionShowing(boolean enable);
+    fun setSectionShowing(enable: Boolean)
 }
