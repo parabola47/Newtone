4e17aa6637f8a2d901c06a756c80edd899176c12
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:24:24 +0300

    RecentlyAddedPlaylistView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/RecentlyAddedPlaylistView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/RecentlyAddedPlaylistView.kt
@@ -1,22 +1,17 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import com.parabola.domain.model.Track;
-import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView;
-
-import java.util.List;
-
-import moxy.MvpView;
-import moxy.viewstate.strategy.alias.AddToEndSingle;
+import com.parabola.domain.model.Track
+import com.parabola.domain.settings.ViewSettingsInteractor.TrackItemView
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
 
 @AddToEndSingle
-public interface RecentlyAddedPlaylistView extends MvpView {
+interface RecentlyAddedPlaylistView : MvpView {
+    fun setCurrentTrack(trackId: Int)
+    fun refreshTracks(tracks: List<Track>)
 
-    void refreshTracks(List<Track> trackList);
-    void setItemViewSettings(TrackItemView viewSettings);
-    void setItemDividerShowing(boolean showed);
+    fun setItemViewSettings(viewSettings: TrackItemView)
+    fun setItemDividerShowing(showed: Boolean)
 
-    void removeTrack(int trackId);
-
-    void setCurrentTrack(int trackId);
-
+    fun removeTrack(trackId: Int)
 }
