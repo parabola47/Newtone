51aae3497f64c9fe8fc1f31d63be2fed113febef
Arslan Khalikov <zizik.zizik@gmail.com>
2022-07-27 19:16:47 +0300

    TabPlaylistView переведён на котлин

--- app/src/main/java/com/parabola/newtone/mvp/view/TabPlaylistView.kt
+++ app/src/main/java/com/parabola/newtone/mvp/view/TabPlaylistView.kt
@@ -1,16 +1,12 @@
-package com.parabola.newtone.mvp.view;
+package com.parabola.newtone.mvp.view
 
-import com.parabola.domain.model.Playlist;
-
-import java.util.List;
-
-import moxy.MvpView;
-import moxy.viewstate.strategy.alias.AddToEndSingle;
+import com.parabola.domain.model.Playlist
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
 
 @AddToEndSingle
-public interface TabPlaylistView extends MvpView {
+interface TabPlaylistView : MvpView {
+    fun refreshPlaylists(playlists: List<Playlist>)
 
-    void refreshPlaylists(List<Playlist> playlists);
-
-    void setItemDividerShowing(boolean showed);
+    fun setItemDividerShowing(showed: Boolean)
 }
