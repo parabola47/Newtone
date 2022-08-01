bcccefacf4a314cde623440f4b52c5a5a9a11a13
Arslan Khalikov <zizik.zizik@gmail.com>
2022-08-01 20:16:43 +0300

    Диалоговый экран EqPresetsSelector переведён на котлин

--- app/src/main/java/com/parabola/newtone/ui/dialog/fx/EqPresetsSelectorDialog.kt
+++ app/src/main/java/com/parabola/newtone/ui/dialog/fx/EqPresetsSelectorDialog.kt
@@ -1,41 +1,31 @@
-package com.parabola.newtone.ui.dialog.fx;
+package com.parabola.newtone.ui.dialog.fx
 
-import android.app.Dialog;
-import android.os.Bundle;
+import android.app.Dialog
+import android.os.Bundle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.parabola.domain.interactor.player.AudioEffectsInteractor
+import com.parabola.newtone.MainApplication
+import com.parabola.newtone.R
+import moxy.MvpAppCompatDialogFragment
+import javax.inject.Inject
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+class EqPresetsSelectorDialog : MvpAppCompatDialogFragment() {
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.parabola.domain.interactor.player.AudioEffectsInteractor;
-import com.parabola.newtone.MainApplication;
-import com.parabola.newtone.R;
+    @Inject
+    lateinit var fxInteractor: AudioEffectsInteractor
 
-import java.util.List;
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        (requireActivity().application as MainApplication).appComponent.inject(this)
+        val presets = fxInteractor.presets
 
-import javax.inject.Inject;
-
-import moxy.MvpAppCompatDialogFragment;
-
-public final class EqPresetsSelectorDialog extends MvpAppCompatDialogFragment {
-
-    @Inject AudioEffectsInteractor fxInteractor;
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        ((MainApplication) requireActivity().getApplication()).getAppComponent().inject(this);
-        List<String> presets = fxInteractor.getPresets();
-        String[] items = new String[presets.size()];
-        items = presets.toArray(items);
-
-        return new MaterialAlertDialogBuilder(requireContext())
-                .setTitle(R.string.preset_selector_dialog_title)
-                .setItems(items, (d, presetIndex) -> {
-                    fxInteractor.usePreset((short) presetIndex);
-                    fxInteractor.setEqEnable(true);
-                })
-                .setNegativeButton(R.string.dialog_cancel, null)
-                .create();
+        return MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.preset_selector_dialog_title)
+            .setItems(presets.toTypedArray()) { _, presetIndex ->
+                fxInteractor.usePreset(presetIndex.toShort())
+                fxInteractor.setEqEnable(true)
+            }
+            .setNegativeButton(R.string.dialog_cancel, null)
+            .create()
     }
+
 }
